e08c5d8d822b987e397971e84a218c49e8b1330a
Grover Richardson <groverrichardson1987@gmail.com>
2020-08-31 16:34:14 -0700

    Fixed error handling and minor bugs.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function watchRandomSubmit() {
         event.preventDefault();
         let input = $('.how-many-dogs').val();
         $(`.image-container.random`).empty();
+        $('.random-error-message').empty();
         try {
             if (input >= 1 && input <= 50) {
                 fetchImages();
@@ -10,7 +11,7 @@ function watchRandomSubmit() {
                 throw new Error('Value needs to be between 1 and 50');
             }
         } catch (error) {
-            $('error-message').text(`this works`);
+            $('.random-error-message').text(error.message);
         }
     });
 }
@@ -18,14 +19,23 @@ function watchRandomSubmit() {
 function watchBreedSubmit() {
     $('button.breed-button').click((event) => {
         event.preventDefault();
+        let input = $('.type-of-dog').val();
+        $('.breed-error-message').empty();
         $(`.image-container.breed`).empty();
-        fetchBreedImages();
+        try {
+            if (input !== '') {
+                fetchBreedImages();
+            } else {
+                throw new Error('Please enter a breed.');
+            }
+        } catch (error) {
+            $('.breed-error-message').text(error.message);
+        }
     });
 }
 
 function fetchImages() {
     let input = $('.how-many-dogs').val();
-    console.log(input);
     fetch(`https://dog.ceo/api/breeds/image/random/${input}`)
         .then((response) => response.json())
         .then((responseJson) => displayRandomImages(responseJson));
@@ -35,12 +45,17 @@ function fetchBreedImages() {
     let input = $('.type-of-dog').val();
     fetch(`https://dog.ceo/api/breed/${input}/images/random`)
         .then((response) => response.json())
-        .then((responseJson) => displayBreedImages(responseJson))
-        .catch((error) => alert(`Something went wrong. Try again later.`));
+        .then((responseJson) => {
+            if (responseJson.status !== 'error') {
+                displayBreedImages(responseJson);
+            } else {
+                throw new Error(responseJson.message);
+            }
+        })
+        .catch((error) => $('.breed-error-message').text(error.message));
 }
 
 function displayRandomImages(responseJson) {
-    console.log(responseJson);
     for (i = 0; i < responseJson.message.length && i < 51; i++) {
         $(`.image-container.random`).append(
             `<img src="${responseJson.message[i]}" class="dog-image">`
@@ -49,7 +64,6 @@ function displayRandomImages(responseJson) {
 }
 
 function displayBreedImages(responseJson) {
-    console.log(responseJson);
     $(`.image-container.breed`).append(
         `<img src="${responseJson.message}" class="dog-image">`
     );
